79ef00cb6bb65b70f9c049214984bc974539333e
shortnd <ococncol@gmail.com>
2019-05-15 23:47:51 -0400

    moving to state tut

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome text="Welcome to React" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +22,17 @@ function App() {
       </header>
     </div>
   );
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <>
+        <h1>{text}</h1>
+      </>
+    )
+  }
 }
 
 export default App;
